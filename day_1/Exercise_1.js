40346295786687e2cfe3b07b3b35ccb5a7ed7491
aamir221 <aamirmanzoor372@gmail.com>
2024-06-23 23:37:01 +0530

    Exercise task 5 done

--- day_1/Exercise_1.js
+++ day_1/Exercise_1.js
@@ -9,3 +9,8 @@ console.log('length of books Array', books1.length);
 console.log('first element', books1[0]);
 console.log('middle element', books1[3]);
 console.log('last element', books1[5]);
+
+
+const mixedDataTypes = [3.14, "Hello", true, 42, null, [1, 2, 3]];
+
+console.log('length of mix Array', mixedDataTypes.length);
