3ada0cb3be6a4e19842f06d5b971b414776ab3ca
aamir221 <aamirmanzoor372@gmail.com>
2024-06-23 23:31:29 +0530

    exercise task 4

--- day_1/Exercise_1.js
+++ day_1/Exercise_1.js
@@ -4,3 +4,8 @@ const books1 = Array('eng', 'hindi', 'urdu', 'science', 'evs', 'maths');
 
 
 console.log('length of books Array', books1.length);
+
+
+console.log('first element', books1[0]);
+console.log('middle element', books1[3]);
+console.log('last element', books1[5]);
